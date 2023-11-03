24c2ede69547e359b825fe8bab7cfda22145595b
Vladislav Valek <vladislav.valek@email.cz>
2023-11-03 21:29:53 +0100

    plot_data.py [FEATURE]: finalize the plotting script

--- hpc01/Exercise_2/2_3/plot_data.py
+++ hpc01/Exercise_2/2_3/plot_data.py
@@ -5,6 +5,7 @@ import numpy as np
 import csv
 
 meas_iterations = 10
+debug = True
 
 def process_file(lines):
     count = 0
@@ -12,18 +13,20 @@ def process_file(lines):
     szs = []
     thrp = []
 
-    for line in lines:
+    for line in lines[2:]:
         parts = line.split()
+        if debug:
+            print("Num of fields in line: ", len(parts))
 
-        if len(parts) >= 7:
-            thr_sum += float(parts[7])
-            count +=1
+        thr_sum += float(parts[5])
+        count +=1
 
-            if (count == meas_iterations):
-                szs.append(int(parts[2]))
-                thrp.append(thr_sum/meas_iterations)
-                thr_sum = 0
-                count = 0
+        if (count == meas_iterations):
+            szs.append(int(parts[2]))
+            thrp.append(thr_sum/meas_iterations)
+            thr_sum = 0
+            count = 0
+
     return szs, thrp
 
 if __name__ == "__main__":
@@ -40,43 +43,65 @@ if __name__ == "__main__":
     # -----------------------------------------------------------------------------------------
     # Read input file
     # -----------------------------------------------------------------------------------------
-    with open('1clus_block.txt', newline='\n') as f_data:
-        lines = f_data.strip().split('\n')
+    with open('1clus_block.txt', "r", newline='\n') as f_data:
+        lines = f_data.readlines()
         one_clus_block_szs,one_clus_block_thrp = process_file(lines)
 
-    with open('2clus_block.txt', newline='\n') as f_data:
-        lines = f_data.strip().split('\n')
+    with open('2clus_block.txt', "r", newline='\n') as f_data:
+        lines = f_data.readlines()
         two_clus_block_szs,two_clus_block_thrp = process_file(lines)
 
-    with open('1clus_nblock.txt', newline='\n') as f_data:
-        lines = f_data.strip().split('\n')
+    with open('1clus_nblock.txt', "r", newline='\n') as f_data:
+        lines = f_data.readlines()
         one_clus_nblock_szs,one_clus_nblock_thrp = process_file(lines)
 
-    with open('2clus_nblock.txt', newline='\n') as f_data:
-        lines = f_data.strip().split('\n')
+    with open('2clus_nblock.txt', "r", newline='\n') as f_data:
+        lines = f_data.readlines()
         two_clus_nblock_szs,two_clus_nblock_thrp = process_file(lines)
 
 
-    for i in range(0,len(one_clus_block_szs) -1):
-        print("{}, {}, {}, {}".format(one_clus_block_szs[i], one_clus_block_thrp[i]))
-    for i in range(0,len(two_clus_block_szs) -1):
-        print("{}, {}, {}, {}".format(two_clus_block_szs[i], two_clus_block_thrp[i]))
+    if debug:
+        for i in range(0,len(one_clus_block_szs)):
+            print("{}, {}, {}".format(one_clus_block_szs[i], one_clus_block_thrp[i], one_clus_nblock_thrp[i]))
+        for i in range(0,len(two_clus_block_szs)):
+            print("{}, {}, {}".format(two_clus_block_szs[i], two_clus_block_thrp[i], two_clus_nblock_thrp[i]))
+        for i in range(0,len(two_clus_nblock_szs)):
+            print("{}, {}, {}, {}".format(two_clus_nblock_szs[i], one_clus_block_thrp[i], two_clus_block_thrp[i], one_clus_block_thrp[i]/two_clus_block_thrp[i]))
 
-    for i in range(0,len(one_clus_nblock_szs) -1):
-        print("{}, {}, {}, {}".format(one_clus_nblock_szs[i], one_clus_nblock_thrp[i]))
-    for i in range(0,len(two_clus_nblock_szs) -1):
-        print("{}, {}, {}, {}".format(two_clus_nblock_szs[i], two_clus_nblock_thrp[i]))
+    fig, ax = plt.subplots(3,1, layout = "constrained", figsize = (16/2.54,20/2.54), dpi = 200)
 
-    fig, ax = plt.subplots()
+    ax[0].plot(one_clus_block_szs, one_clus_block_thrp, marker='x', linestyle='-', color='blue', label='Blocking send')
+    ax[0].plot(one_clus_nblock_szs, one_clus_nblock_thrp, marker='x', linestyle='-', color='red', label='Non-blocking send')
+    ax[0].set_title("MPI Throughput on a single cluser")
+    ax[0].grid(True)
+    ax[0].set_xlabel("Message size [B]")
+    ax[0].set_ylabel("Througphut [MBps]")
+    ax[0].legend()
+    ax[0].set_xscale("log")
+    ax[0].set_yscale("log")
+    ax[0].set_xlim(1, 10**7)
 
-    ax.plot(packet_sizes, throughputs, marker='o', linestyle='-', color='b', label='Throughput')
+    ax[1].plot(two_clus_block_szs, two_clus_block_thrp, marker='x', linestyle='-', color='green', label='Blocking send')
+    ax[1].plot(two_clus_nblock_szs, two_clus_nblock_thrp, marker='x', linestyle='-', color='orange', label='Non-blocking send')
+    ax[1].set_title("MPI Throughput between two clusters")
+    ax[1].grid(True)
+    ax[1].set_xlabel("Message size [B]")
+    ax[1].set_ylabel("Througphut [MBps]")
+    ax[1].legend()
+    ax[1].set_xscale("log")
+    ax[1].set_yscale("log")
+    ax[1].set_xlim(1, 10**7)
 
-    # plt.xscale('log')  # Set X-axis to a logarithmic scale
-    # plt.yscale('log')  # Set Y-axis to a logarithmic scale
-    ax.grid(True)
-    ax.set_xlabel("Packet size [B]")
-    ax.set_ylabel("Througphut [MBps]")
+    ax[2].plot(one_clus_block_szs, one_clus_block_thrp, marker='x', linestyle='-', color='blue', label='One cluster')
+    ax[2].plot(two_clus_block_szs, two_clus_block_thrp, marker='x', linestyle='-', color='green', label='Two clusters')
+    ax[2].set_title("MPI Throughput on one vs. two clusters (blocking send)")
+    ax[2].grid(True)
+    ax[2].set_xlabel("Message size [B]")
+    ax[2].set_ylabel("Througphut [MBps]")
+    ax[2].legend()
+    ax[2].set_xscale("log")
+    ax[2].set_yscale("log")
+    ax[2].set_xlim(1, 10**7)
 
-    ax.legend()
-
-    plt.show()
+    plt.savefig("image.png")
+    # plt.show()
