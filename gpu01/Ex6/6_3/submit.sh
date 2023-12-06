d5f482deefe34217f59506a29cf3abb3d83e9ea4
gpucomp1 <gpucomp1@ceg-octane.ziti.uni-heidelberg.de>
2023-12-06 18:18:03 +0100

    files 6_3

---
+++ gpu01/Ex6/6_3/submit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=matMul
+#SBATCH --partition=exercise   # partition (queue)
+#SBATCH -t 0-01:00              # time limit: (D-HH:MM) 
+#SBATCH --gres=gpu:1
+#SBATCH -o slurm.out
+#SBATCH --error=slurm.err      # file to collect standard errors
+
+module load devtoolset/10 cuda/11.6
+
+make
+
+./bin/reduction --size 1024 --tilewidth 32 --shared
+
+
+# for ((size = 32; size <= 1024; size*=2))
+# do
+#     srun ./bin/matMul -s $size -t 32 --shared >> out_5_3_3.txt
+# done
